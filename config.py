f2cc833aafe8f64b507e2d9b7f063c749daeacbf
tapnogaming01 <tapnogaming01@gmail.com>
2026-08-26 19:51:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME", "YourBotUsername")
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://...")
 
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 123456789))
-SOURCE_CHANNEL = int(os.environ.get("SOURCE_CHANNEL", -100123456789))
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100987654321))
+SOURCE_CHANNEL = int(os.environ.get("SOURCE_CHANNEL", -1004477265073))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1003814751875))
 
 DEVELOPER_NAME = "Kaluu"
-DEVELOPER_LINK = "https://t.me/yourdeveloperusername"
-SUPPORT_GROUP = "https://t.me/yoursupportgroup"
-MAIN_CHANNEL = "https://t.me/yourmainchannel"
+DEVELOPER_LINK = "https://t.me/KCXRY"
+SUPPORT_GROUP = "https://t.me/pratilipifm0900"
+MAIN_CHANNEL = "https://t.me/freestoryhubMR"
 
 PORT = int(os.environ.get("PORT", 8080))
