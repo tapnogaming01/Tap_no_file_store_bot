6e8a3f1d6afc3ce2b36d469fb5198226d7d7ae8f
tapnogaming01 <tapnogaming01@gmail.com>
2026-08-26 19:12:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 1234567))
+API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "YourBotUsername")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://...")
+
+ADMIN_ID = int(os.environ.get("ADMIN_ID", 123456789))
+SOURCE_CHANNEL = int(os.environ.get("SOURCE_CHANNEL", -100123456789))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100987654321))
+
+DEVELOPER_NAME = "Kaluu"
+DEVELOPER_LINK = "https://t.me/yourdeveloperusername"
+SUPPORT_GROUP = "https://t.me/yoursupportgroup"
+MAIN_CHANNEL = "https://t.me/yourmainchannel"
+
+PORT = int(os.environ.get("PORT", 8080))
